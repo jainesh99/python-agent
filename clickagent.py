be2a1e93a843c74f0d235b373312072dd758cd76
Jainesh J Hira <jaineshster@gmail.com>
2018-07-24 20:51:59 +0200

    Added the clickagent and keyboard agent, the click agent combined to do everything

---
+++ clickagent.py
@@ -0,0 +1,41 @@
+from pynput import mouse, keyboard
+import pyautogui
+
+
+def on_press(key):
+    try:
+        print('alphanumeric key {0} pressed'.format(
+            key.char))
+    except AttributeError:
+        print('special key {0} pressed'.format(
+            key))
+
+
+key_listener = keyboard.Listener(on_release=on_press)
+key_listener.start()
+
+
+def on_move(x, y):
+    print('Pointer moved to {0}'.format(
+        (x, y)))
+
+
+def on_click(x, y, button, pressed):
+    print('{0} at {1}'.format(
+        'Pressed' if pressed else 'Released',
+        (x, y)))
+    pyautogui.screenshot('test.png')
+
+
+def on_scroll(x, y, dx, dy):
+    print('Scrolled {0} at {1}'.format(
+        'down' if dy < 0 else 'up',
+        (x, y)))
+
+
+# Collect events until released
+with mouse.Listener(
+        on_move=on_move,
+        on_click=on_click,
+        on_scroll=on_scroll) as listener:
+    listener.join()
