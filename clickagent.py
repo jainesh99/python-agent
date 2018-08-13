f4f962676a655f32c9262460e3488c229af577c6
Jainesh J Hira <jaineshster@gmail.com>
2018-08-13 18:45:44 +0200

    Changed the printing for the logging to something meaningful.

--- clickagent.py
+++ clickagent.py
@@ -4,33 +4,28 @@ import pyautogui
 
 def on_press(key):
     try:
-        print('alphanumeric key {0} pressed'.format(
-            key.char))
+        print('K ' + key.char)
     except AttributeError:
-        print('special key {0} pressed'.format(
-            key))
+        print('S ' + str(key).replace('Key.', ''))
 
 
-key_listener = keyboard.Listener(on_release=on_press)
+key_listener = keyboard.Listener(on_press=on_press)
 key_listener.start()
 
 
 def on_move(x, y):
-    print('Pointer moved to {0}'.format(
-        (x, y)))
+    print('M ' + str(x) + ' ' + str(y))
 
 
 def on_click(x, y, button, pressed):
-    print('{0} at {1}'.format(
-        'Pressed' if pressed else 'Released',
-        (x, y)))
-    pyautogui.screenshot('test.png')
+    print(str(button).replace('Button.', '').upper()[0] + ' ' + str(x) + ' ' + str(y))
 
 
 def on_scroll(x, y, dx, dy):
-    print('Scrolled {0} at {1}'.format(
-        'down' if dy < 0 else 'up',
-        (x, y)))
+    if dy < 0:
+        print('D' + str(abs(dy)))
+    else:
+        print('U' + str(dy))
 
 
 # Collect events until released
